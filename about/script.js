246646d74106922844c3567e05e0dab0abde818d
slob16 <sian@wetalentedfew.com>
2025-01-13 15:23:12 +0000

    Update script.js

--- about/script.js
+++ about/script.js
@@ -505,3 +505,10 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+VanillaTilt.init(document.querySelector(".glassmorphic-card"), {
+  max: 15,
+  speed: 200,
+  glare: true,
+  "max-glare": 1,
+});
